bb810e0c3dfabdf7ac610dc066c66a82c9ac40e1
trung nam <cfpro99vn@gmail.com>
2022-08-22 01:23:24 +0700

    add dropdown

--- src/pages/landing/Filter/index.tsx
+++ src/pages/landing/Filter/index.tsx
@@ -1,14 +1,117 @@
 import clsx from 'clsx'
 import styles from './index.module.scss'
+import { DownOutlined } from '@ant-design/icons'
+import { Button, Checkbox, Dropdown, Form, Menu } from 'antd'
+import Input from 'antd/lib/input/Input'
+import type { CheckboxValueType } from 'antd/es/checkbox/Group'
+
 interface IFilter {
   className?: string
 }
+
+const menu = (
+  <Menu
+    items={[
+      {
+        key: '1',
+        label: <span>New Car ( Authorised Dealer)</span>,
+      },
+      {
+        key: '2',
+        label: <span>New Car ( Parallel Importer )</span>,
+      },
+      {
+        key: '3',
+        label: <span>Used Cars</span>,
+      },
+    ]}
+  />
+)
+
+const menu2 = (
+  <div className={styles.menu__range__price}>
+    <span>Price Range</span>
+    <div>
+      <Form className={styles.form__rangge__price}>
+        <Form.Item label="Min" className={styles['form__item--half']}>
+          <Input bordered={false} />
+        </Form.Item>
+        <span>-</span>
+        <Form.Item label="Max" className={styles['form__item--half']}>
+          <Input bordered={false} />
+        </Form.Item>
+      </Form>
+      <div>
+        <Button className={styles.btn__reset}>Reset</Button>
+        <Button type="primary" danger>
+          Save
+        </Button>
+      </div>
+    </div>
+  </div>
+)
+const options = [
+  { label: 'Apple', value: 'Apple' },
+  { label: 'Pear', value: 'Pear' },
+  { label: 'Orange', value: 'Orange' },
+  { label: 'Apple', value: 'Apple' },
+  { label: 'Pear', value: 'Pear' },
+  { label: 'Orange', value: 'Orange' },
+  { label: 'Apple', value: 'Apple' },
+  { label: 'Pear', value: 'Pear' },
+  { label: 'Orange', value: 'Orange' },
+]
+
+const onChange = (checkedValues: CheckboxValueType[]) => {
+  console.log('checked = ', checkedValues)
+}
+const menu3 = (
+  <div className={styles.menu__range__price}>
+    <span>Price Range</span>
+    <div>
+      <Checkbox.Group options={options} defaultValue={['Apple']} onChange={onChange} />
+      <div>
+        <Button className={styles.btn__reset}>Reset</Button>
+        <Button type="primary" danger>
+          Save
+        </Button>
+      </div>
+    </div>
+  </div>
+)
 export const Filter = ({ className }: IFilter) => {
   return (
     <div className={clsx(styles.container, className)}>
       <div>
-        <span>New/ Used</span>
+        <div>
+          <span>New/ Used</span>
+          <Dropdown overlay={menu}>
+            <div className={styles.item}>
+              <span>New Cars</span>
+              <DownOutlined />
+            </div>
+          </Dropdown>
+        </div>
+        <div>
+          <span>Price Range</span>
+          <Dropdown overlay={menu2}>
+            <div className={styles.item}>
+              <span>New Cars</span>
+              <DownOutlined />
+            </div>
+          </Dropdown>
+        </div>
+        <div>
+          <span>Vehicle Type</span>
+          <Dropdown overlay={menu3}>
+            <div className={styles.item}>
+              <span>New Cars</span>
+              <DownOutlined />
+            </div>
+          </Dropdown>
+        </div>
       </div>
+      <div>{menu3}</div>
     </div>
   )
 }
--- src/pages/landing/Footer/index.tsx
+++ src/pages/landing/Footer/index.tsx
@@ -4,6 +4,7 @@ import { DownOutlined } from '@ant-design/icons'
 import styles from './index.module.scss'
 import { FacebookIcon, LinkedinIcon, TelegramIcon, YoutubeIcon } from '../../../assets/icons'
 import { IconWrap } from '../../../components'
+import ucarMini from '../../../assets/images/ucarMini.png'
 export const Footer = () => {
   const onChange = () => {
     console.log('onChange')
@@ -18,48 +19,61 @@ export const Footer = () => {
     />
   )
   return (
-    <div className={styles.container}>
-      <div>
-        <img src={CarbuyerLogo2} alt="" />
+    <>
+      <div className={styles.container}>
+        <div>
+          <img src={CarbuyerLogo2} alt="" />
+        </div>
+        <div>
+          <span>71 Ayer Rajah Crescent, #06-14, Singapore 139951</span>
+          <span>+65 8808 7905</span>
+          <span>hello@carbuyer.com.sg</span>
+        </div>
+        <Collapse onChange={onChange} ghost>
+          <Collapse.Panel showArrow={false} header="About" key="1" extra={genExtra()}>
+            <p>About</p>
+          </Collapse.Panel>
+          <Collapse.Panel showArrow={false} header="Services" key="2" extra={genExtra()}>
+            <p>Services</p>
+          </Collapse.Panel>
+          <Collapse.Panel showArrow={false} header="Resources" key="3" extra={genExtra()}>
+            <p>Resources</p>
+          </Collapse.Panel>
+          <Collapse.Panel showArrow={false} header="Legal" key="4" extra={genExtra()}>
+            <p>Legal</p>
+          </Collapse.Panel>
+        </Collapse>
+        <div className={styles.subscribe}>
+          <span>
+            Get the <strong>latest</strong> automotive news sent to your inbox!
+          </span>
+          <Input.Group compact>
+            <Input style={{ width: 'calc(100% - 92px)' }} placeholder="Enter your email" />
+            <Button type="primary" danger>
+              Subscribe
+            </Button>
+          </Input.Group>
+        </div>
+        <div className={styles.social}>
+          <span>Follow us</span>
+          <IconWrap icon={<YoutubeIcon />} />
+          <IconWrap icon={<FacebookIcon />} />
+          <IconWrap icon={<TelegramIcon />} />
+          <IconWrap icon={<LinkedinIcon />} />
+          <IconWrap icon={<YoutubeIcon />} />
+        </div>
+        <div className={styles.ucar__description}>
+          <div>
+            <img src={ucarMini} alt="" />
+          </div>
+          <span>
+            CarBuyer Pte Ltd and the CarBuyer Singapore brand <br /> are wholly owned by UCARS Pte Ltd
+          </span>
+        </div>
       </div>
-      <div>
-        <span>71 Ayer Rajah Crescent, #06-14, Singapore 139951</span>
-        <span>+65 8808 7905</span>
-        <span>hello@carbuyer.com.sg</span>
+      <div className={styles.copyright}>
+        <span>© 2022. All rights reserved.</span>
       </div>
-      <Collapse onChange={onChange} ghost>
-        <Collapse.Panel showArrow={false} header="About" key="1" extra={genExtra()}>
-          <p>About</p>
-        </Collapse.Panel>
-        <Collapse.Panel showArrow={false} header="Services" key="2" extra={genExtra()}>
-          <p>Services</p>
-        </Collapse.Panel>
-        <Collapse.Panel showArrow={false} header="Resources" key="3" extra={genExtra()}>
-          <p>Resources</p>
-        </Collapse.Panel>
-        <Collapse.Panel showArrow={false} header="Legal" key="4" extra={genExtra()}>
-          <p>Legal</p>
-        </Collapse.Panel>
-      </Collapse>
-      <div className={styles.subscribe}>
-        <span>
-          Get the <strong>latest</strong> automotive news sent to your inbox!
-        </span>
-        <Input.Group compact>
-          <Input style={{ width: 'calc(100% - 92px)' }} placeholder="Enter your email" />
-          <Button type="primary" danger>
-            Subscribe
-          </Button>
-        </Input.Group>
-      </div>
-      <div className={styles.social}>
-        <span>Follow us</span>
-        <IconWrap icon={<YoutubeIcon />} />
-        <IconWrap icon={<FacebookIcon />} />
-        <IconWrap icon={<TelegramIcon />} />
-        <IconWrap icon={<LinkedinIcon />} />
-        <IconWrap icon={<YoutubeIcon />} />
-      </div>
-    </div>
+    </>
   )
 }
